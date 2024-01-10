ae21f2fcceb0f596d42c39cc751a4dec04a65a4f
AdityaBVerma <adityabverma1@gmail.com>
2024-01-10 22:06:30 +0530

    user model bcrypt aggregation pipeline import jwt access and refresh token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,3 +1,8 @@
+import bcrypt from "bcrypt";// this is used to hash the pass and also compare it to the ones provided by the user
+import jwt from "jsonwebtoken";// this lib creates jwt token we have to pass some variables
+
+// *** object model ***
+
 import mongoose, {Schema} from "mongoose";
 
 const userSchema = new Schema({
@@ -7,7 +12,7 @@ const userSchema = new Schema({
         unique: true,
         lowercase: true,
         trim: true,
-        index: true
+        index: true // helps in searching optimizes code
     },
     email: {
         type: String,
@@ -30,6 +35,12 @@ const userSchema = new Schema({
         type: String, // cloudinary url
         required: true
     },
+    watchHistory:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
     password: {
         type: String,
         required: [true , "Password is required "]
@@ -38,5 +49,53 @@ const userSchema = new Schema({
         type: String
     }
 }, {timestamps : true})
+
+// ***** hooks middlewares and bcrypt *****
+
+userSchema.pre("save", async function(next) { // pre is a middleware fun and save is a document function
+    // here we use functions in place of callback as it does not have the access to this.password
+
+    if(!this.isModified("password")) return next();
+    
+    this.password = bcrypt.hash(this.password , 10)
+    next()
+})
+
+// to check pass provided by user is matching with hashed password ***
+// we make our custom hooks(save, validate, etc) ***
+// if isPasswordCorrect(document function which is called by this.constructor) is not there it will be created by mongoose ***
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+    // returns value in boolean here password is the one which is given by user
+}
+
+// **** jwt *****
+
+userSchema.methods.generateAccessTokens = function(){
+    jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshTokens = function(){
+    jwt.sign(
+        {
+            _id: this._id, // as it keeps on refreshing hence we only keep id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 
 export const User = mongoose.model("User", userSchema)
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,6 +1,42 @@
 import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
 
-const userSchema = new Schema({
+const videoSchema = new Schema({
+    videofile:{
+        type: String, // cloudinary url
+        required: true
+    },
+    thumbnail: {
+        type: String, // cloudinary url
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    discription: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number, // as this will come from cloudinary url
+        required: true
+    },
+    views: {
+        type: Number,
+        default: 0,
+    },
+    isPublished: {
+        type: Boolean,
+        default: true
+    },
+    owner:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
 }, {timestamps : true})
 
-export const User = mongoose.model("User", userSchema)
+// **** hooks ****  
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
