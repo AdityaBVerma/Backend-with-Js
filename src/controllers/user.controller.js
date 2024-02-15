5dae18c463c8e70fb7793c67983fd83cbcd6d8df
AdityaBVerma <adityabverma1@gmail.com>
2024-02-15 08:18:11 +0530

    fixed all register cintroller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,8 @@
 import asyncHandler from "../utils/asyncHandler.js"
-import ApiError from "../utils/ApiError.js"
+import {ApiError} from "../utils/ApiError.js"
 import { User } from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
-import ApiResponse from "../utils/ApiResponse.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
 
 const registerUser = asyncHandler( async (req, res) => {
     // get user details from front end 
@@ -34,13 +34,19 @@ const registerUser = asyncHandler( async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let coverImageLocalPath
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar is required")
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
 
     if (!avatar) {
         throw new ApiError(409, "Avatar is required")
@@ -52,7 +58,7 @@ const registerUser = asyncHandler( async (req, res) => {
         coverImage:coverImage?.url || "",
         email,
         password,
-        username: username.toLoweCase()
+        username: username.toLowerCase()
     })
 
     const createdUser = await User.findById(user._id).select(
