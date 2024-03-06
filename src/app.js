4d9954656ec139c7fae52ac550e3682f41ea4b94
AdityaBVerma <adityabverma1@gmail.com>
2024-03-06 22:09:57 +0530

    subscription controllers and routes

--- src/app.js
+++ src/app.js
@@ -18,9 +18,13 @@ app.use(cookieParser())
 import userRouter from "./routes/user.routes.js"
 import videoRouter from "./routes/video.routes.js"
 import tweetRouter from "./routes/tweet.routes.js"
+import subscriptionRouter from "./routes/subscription.routes.js"
+
 // routes declare
 app.use("/api/v1/users", userRouter) // prefix will be /users
 app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/tweet", tweetRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
+
 
 export { app }
